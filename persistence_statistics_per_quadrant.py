67166bf8c4c8c36dd61693c17d2798f1a7aae9be
yp4g14 <yp4g14@soton.ac.uk>
2021-08-19 22:35:47 +0100

    working on stats unit tests

---
+++ persistence_statistics_per_quadrant.py
@@ -0,0 +1,263 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+from os import listdir, mkdir
+from os.path import isfile, join, exists
+
+def distribution_stats_column(df, dim, colname):
+    """Creates a dictionary summary of distribution statistics for a numeric column in a pandas dataframe.
+
+    Args:
+        df (pandas DataFrame): dataframe with column specified by "colname" e.g. "birth"
+        dim : [0,1] dimension of persistence diagram df belongs to.
+        colname : numeric column name as string in df to be summarized with statistics.
+
+    Returns:
+        dictionary: statistics keyed by {dim}_{statistic}_{colname}
+    """
+    summary = {}
+    summary[f"{dim}_avg_{colname}"] = df[colname].mean()
+    summary[f"{dim}_max_{colname}"] = df[colname].max()
+    summary[f"{dim}_min_{colname}"] = df[colname].min()
+    summary[f"{dim}_stddev_{colname}"] = df[colname].std()
+    summary[f"{dim}_skew_{colname}"] = df[colname].skew()
+    summary[f"{dim}_kurtosis_{colname}"] = df[colname].kurtosis()
+    summary[f"{dim}_percentile_25_{colname}"] = df[colname].quantile(q=0.25)
+    summary[f"{dim}_percentile_75_{colname}"] = df[colname].quantile(q=0.75)
+    summary[f"{dim}_iqr_{colname}"] = summary[f"{dim}_percentile_75_{colname}"] - summary[f"{dim}_percentile_25_{colname}"]
+    for perc in [round(0.1*i,2) for i in range(1,10)]:
+        summary[f"{dim}_percentile_{round(100*perc)}_{colname}"] = df[colname].quantile(perc)
+    return summary
+
+def quadrant_statistics(
+    intervals,
+    dim,
+    filename,
+    radius_split=-2):
+    """Takes in an array of birth, death intervals and calculates statistics
+    per quadrant (1,2,3 as 4 naturally empty) for each dimension in dim.
+
+    Args:
+        intervals (numpy array): persistence intervals as array with two columns with births, deaths
+        dim (int): persistence diagram for dimension in [0,1]
+        filename (string): [description]
+        radius_split (int, optional): (for quadrant 2 dim 0 only) will calculate the number of births less than or greater than or equal to value.
+            Defaults to -2.
+    Returns:
+        pandas DataFrame: topological statistics calculated per quadrant.
+    """
+
+    # split into quadrants:
+    Q1 = intervals.loc[intervals["birth"] >= 0]
+    Q2 = intervals.loc[(intervals["birth"] < 0) & (intervals["death"] >= 0)]
+    Q3 = intervals.loc[(intervals["birth"] < 0) & (intervals["death"] < 0)]
+
+    intervals_quadrants = [Q1,Q2,Q3]
+    stats_list = []
+    # for each quadrant calculate the statistics in a dictionary
+    for i in range(3):
+        quadrant = intervals_quadrants[i]
+        stats = {}
+        stats["name"] = filename
+        stats["quadrant"] = i+1
+        if quadrant.shape[0] > 0:
+            stats[f"{dim}_num_points"] = quadrant.shape[0]
+            stats[f"{dim}_avg_birth"] = quadrant["birth"].sum()/quadrant.shape[0]
+            # remove inf death
+            finite_quadrant = quadrant.copy(deep=True)
+            finite_quadrant = finite_quadrant.loc[finite_quadrant["death"]!=np.inf]
+            stats[f"{dim}_avg_death"] = finite_quadrant["death"].sum()/finite_quadrant.shape[0]
+            birth_stats = distribution_stats_column(quadrant,
+                                            dim,
+                                            "birth")
+            stats.update(birth_stats)
+            death_stats = distribution_stats_column(finite_quadrant,
+                                            dim,
+                                            "death")
+            stats.update(death_stats)
+            
+            #lifetime = death - birth
+            finite_quadrant["lifetime"] = finite_quadrant["death"] - finite_quadrant["birth"]
+            #total persistance is the sum of all lifetimes
+            stats[f"{dim}_total_persistence"] = finite_quadrant.sum()["lifetime"]
+            # normalized_lifespan = (death - birth)/ sum_all(death-birth)
+            finite_quadrant["normalized_lifespan"] = finite_quadrant["lifetime"]/stats[f"{dim}_total_persistence"]
+            #let p be the normalised lifespan - persistent entropy can be viewed as the diversity of lifespans
+            finite_quadrant["plogp"] = finite_quadrant["normalized_lifespan"] * np.log(finite_quadrant["normalized_lifespan"])
+            stats[f"{dim}_pers_entropy"] = - finite_quadrant["plogp"].sum()
+            #number of births in quadrant 2 dim 0, less than equal to radius split, greater than radius split
+            if (i+1 == 2) and (dim==0):
+                stats[f"{dim}_num_points_less_eq_{radius_split}"] = quadrant.loc[quadrant["birth"]<=radius_split].shape[0]
+                stats[f"{dim}_num_points_greater_{radius_split}"] = quadrant.loc[quadrant["birth"]>radius_split].shape[0]
+
+            stats = pd.DataFrame([stats])
+            stats_list.append(stats)
+
+    # combine all stats into a DataFrame
+    if len(stats_list) != 0:
+        stats_df = pd.concat(stats_list)
+        return stats_df
+    else:
+        return None
+
+def combine_stats_files(
+    path,
+    save_path,
+    save_name):
+    """Combines statistics per image patch into a DataFrame of all statistics
+    
+    Args:
+        path (string): location of stats files to combine
+        save_path (string): 
+        save_name (string): filename to save as csv
+
+    Returns:
+        pandas DatFrame: all statistics in a dataframe
+    """
+    files = [file for file in listdir(path) if isfile(join(path, file))]
+    # files = check_ext(files, ["csv"])
+    stats = []
+    columns = set()
+    for name in files:
+        df = pd.read_csv(path+name)
+        stats.append(df)
+        columns = columns|set(df.columns)
+    # order the columns the same in all files before join
+    for i in range(len(stats)):
+        cols_present = set(stats[i].columns)
+        cols_missing = columns - cols_present
+        for col in list(cols_missing):
+            stats[i][col] = None
+        stats[i] = stats[i][list(columns)]
+    stats = pd.concat(stats, ignore_index=True)
+    try:
+        stats = stats.drop("Unnamed: 0", axis=1)
+        stats = stats.drop("name.1", axis=1)
+    except:
+        pass
+    if not exists(path):
+        mkdir(path)
+    stats.to_csv(f"{save_path}{save_name}")
+    return stats
+
+# def quad_stats_from_intervals_csv(interval_path,
+#                                   interval_files,
+#                                   stats_path,
+#                                   image_filename,
+#                                   logger,
+#                                   x,
+#                                   birth_limits=None,
+#                                   confidence_band=False,
+#                                   conf_c=None,
+#                                   single_dim=None):
+#     """
+#     Takes paths to interval files of persistence diagrams 
+#     for an image with name image_name and outputs the summary statistics
+#     saving them in stats_path.
+#     Can optionally apply a confidence band death > birth + 2*conf_c 
+#     Usually takes 2 files for dim 0, dim 1 intervals, can be used for a single dim. 
+    
+#     Parameters
+#     ----------
+#     interval_path (STRING) :
+#         path to interval files 
+#     interval_files (LIST): 
+#         list containing 2 interval filenames for dim 0 then dim 1 (in that order)
+#     stats_path (STRING): 
+#         location to save the statistics calculated
+#     image_filename (STRING):
+#         string of filename which the intervals correspond to 
+#     logger (logging.Logger): 
+#     x (int): 
+#         length of file extension to remove from name
+#     birth_limits (default None):
+#         restrict to intervals with a birth value in this range
+#     confidence_band (bool, optional): 
+#         optionally adds a confidence band. Defaults to False.
+#     conf_c (INT, optional): 
+#         width of confidence_band when confidence_band is True, otherwise ignored. Defaults to None.
+#     single_dim (INT, optional): 
+#         If calculating the statistics for just dim 0, set to 0. Defaults to None.
+
+#     Returns
+#     -------
+#     stats : pandas DataFrame containing statistics per quadrant
+#     """
+
+#     # if calculating quadrant stats for only one dimension of persistence diagram 
+#     if single_dim is not None:
+        
+#         #read in interval csv files
+#         intervals = pd.read_csv(interval_path+interval_files[0],
+#                                 names=["birth", "death"])
+        
+#         if confidence_band == True:
+#             intervals = intervals.loc[intervals["death"] > intervals["birth"]+2*conf_c]
+#         if birth_limits is not None:
+#             birth_limits.sort()
+#             intervals = intervals.loc[intervals["birth"] >= birth_limits[0]]
+#             intervals = intervals.loc[intervals["birth"] <= birth_limits[1]]
+#             if intervals.shape[0] == 0:
+#                 logger.warning(f"No stats in birth limits {birth_limits}")
+#                 pass
+#         stats = pd.DataFrame()
+#         if intervals.shape[0] != 0:
+#             stats_intervals = quadrant_statistics(intervals,
+#                                                 single_dim,
+#                                                 image_filename[:-x],
+#                                                 logger)
+            
+#             stats_intervals.to_csv(f"{stats_path}{image_filename[:-x]}_conf_{conf_c}_statistics.csv")
+#         else:
+#             stats_intervals = None
+#         return stats_intervals
+        
+#     else:
+#         #read in interval csv files
+#         intervals_0 = pd.read_csv(interval_path+interval_files[0],
+#                                   names=["birth", "death"])
+#         intervals_1 = pd.read_csv(interval_path+interval_files[1],
+#                                   names=["birth", "death"])
+    
+#         if confidence_band == True:
+#             intervals_0 = intervals_0.loc[intervals_0["death"] > intervals_0["birth"]+2*conf_c]
+#             intervals_1 = intervals_1.loc[intervals_1["death"] > intervals_1["birth"]+2*conf_c]
+#         if birth_limits is not None:
+#             birth_limits.sort()
+#             intervals_0 = intervals_0.loc[intervals_0["birth"] >= birth_limits[0]]
+#             intervals_0 = intervals_0.loc[intervals_0["birth"] <= birth_limits[1]]
+#             intervals_1 = intervals_1.loc[intervals_1["birth"] >= birth_limits[0]]
+#             intervals_1 = intervals_1.loc[intervals_1["birth"] <= birth_limits[1]]
+#         if intervals_0.shape[0] == 0:
+#             if intervals_1.shape[0] == 0:
+#                 logger.warning(f"No stats in birth limits {birth_limits}")
+#                 pass
+
+#         stats = pd.DataFrame()
+#         if intervals_0.shape[0] != 0:
+#             stats_intervals_0 = quadrant_statistics(intervals_0,
+#                                                     0,
+#                                                     image_filename[:-x],
+#                                                     logger)
+#         else:
+#             stats_intervals_0 = None
+#         if intervals_1.shape[1] != 0:
+#             stats_intervals_1 = quadrant_statistics(intervals_1,
+#                                                     1,
+#                                                     image_filename[:-x],
+#                                                     logger)
+#         else:
+#             stats_intervals_1 = None
+
+#         if stats_intervals_0 is not None:
+#             if stats_intervals_1 is not None:
+#                 stats = stats_intervals_0.merge(stats_intervals_1,
+#                                                 how="outer",
+#                                                 on=["name","quadrant"])
+#             else:
+#                 stats = stats_intervals_0
+#         elif stats_intervals_1 is not None:
+#             stats = stats_intervals_1
+#         if stats is not None:
+#             stats.to_csv(f"{stats_path}{image_filename[:-x]}_birthlimits_{birth_limits}_conf_{conf_c}_statistics.csv")
+#         return stats
---
+++ ut_persistence_statistics_per_quadrant.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+from importlib import reload
+from os import rmdir, remove, listdir
+from os.path import exists, isfile
+from math import sqrt
+import persistence_statistics_per_quadrant as stats
+reload(stats)
+from scipy.stats import skew, kurtosis
+# TEST - quadrant_statistics
+######################################################
+test_intervals = np.array([
+    [0, 0],
+    [0, 4],
+    [2, 4],
+    [4, 6],
+    [-3, 3],
+    [-2, 4],
+    [-1, 0],
+    [-5, -1],
+    [-4, -2]])
+
+test_intervals = pd.DataFrame(test_intervals, columns=["birth","death"])
+test_intervals['birth'].skew()
+filename = "test.tif"
+save_path = "C:/test_"
+quadrants = [1,2,3]
+num_points = [4,3,2]
+avg_birth = [6/4,-2,-9/2]
+avg_death = [14/4,7/3,-3/2]
+max_birth = [4,-1,-4]
+min_birth = [0,-3,-5]
+stdev_birth = [sqrt(11/3),1.,sqrt(0.5)]
+skew_birth = [skew(np.array([0,0,2,4]),bias=False), skew(np.array([-3,-2,-1]),bias=False),skew(np.array([-5,-4]),bias=False)]
+kurt_birth = [kurtosis(np.array([0,0,2,4]),bias=False), kurtosis(np.array([-3,-2,-1]),bias=False),kurtosis(np.array([-5,-4]),bias=False)]
+birth_percentiles_q1 = np.percentile(np.array([0,0,2,4]),[10*q for q in list(range(1,10))+[2.5,7.5]])
+birth_percentiles_q2 = np.percentile(np.array([-3,-2,-1]),[10*q for q in list(range(1,10))+[2.5,7.5]])
+birth_percentiles_q3 = np.percentile(np.array([-5,-4]),[10*q for q in list(range(1,10))+[2.5,7.5]])
+birth_percentiles = np.array([birth_percentiles_q1,birth_percentiles_q2,birth_percentiles_q3])
+birth_percentiles = pd.DataFrame(
+    birth_percentiles,
+    columns=['0_percentile_10_birth','0_percentile_20_birth','0_percentile_30_birth',
+    '0_percentile_40_birth', '0_percentile_50_birth', '0_percentile_60_birth',
+    '0_percentile_70_birth', '0_percentile_80_birth', '0_percentile_90_birth',
+    '0_percentile_25_birth','0_percentile_75_birth'])
+birth_percentiles['0_iqr_birth'] = birth_percentiles['0_percentile_75_birth'] - birth_percentiles['0_percentile_25_birth']
+
+max_death = [6,4,-1]
+min_death = [0,0,-2]
+
+test_stats = pd.DataFrame(
+    np.array([quadrants,num_points,avg_birth,avg_death,max_birth,min_birth,stdev_birth]).T,
+    columns=['quadrant','0_num_points','0_avg_birth','0_avg_death','0_max_birth','0_min_birth','0_stddev_birth'])
+
+test_stats['filename'] = 'test.tif'
+stat_df = stats.quadrant_statistics(test_intervals, 0,filename,radius_split=-2)
+
+
+# check shape of stats
+
+#check for NaNs
+
+# TEST - distribution_stats_column
+######################################################
+
+
+# TEST - combine_stats_files
+######################################################
+
+
+# # TEST - quad_stats_from_intervals_csv
+# ######################################################
+# # check it creates necessary paths
+# assert exists(save_path), "save_path does not exist for statistics"
+
+# # check it creates necessary files
+# assert isfile(save_path+"statistics.csv"), "stats file does not exist"
+
+# # remove test files from system
+# remove(save_path+"statistics.csv")
+# rmdir(save_path+"idiagrams/")
