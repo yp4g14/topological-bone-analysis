4b44ce62af8e2ca40bc91ba907bd4b2ed47a30a6
yp4g14 <yp4g14@soton.ac.uk>
2021-08-19 23:37:43 +0100

    formatting pre processing and unit test

--- preprocessing_images.py
+++ preprocessing_images.py
@@ -7,8 +7,10 @@ from itertools import product
 
 def trim(
     image,
-    edge_val=0):
-    """Trims an image array removing full rows and columns containing edge_val from the borders
+    edge_val=0
+    ):
+    """Trims an image array removing full rows and columns containing edge_val
+     from the borders
 
     Args:
         image (numpy array): image as numpy array
@@ -17,26 +19,31 @@ def trim(
     Returns:
         image as numpy array: trimmed image
     """
-    n_rows, n_cols = image.shape
-    col_sums = image.sum(axis=0)
-    row_sums = image.sum(axis=1)
-    non_zero_cols = np.where(col_sums!=n_rows*edge_val)[0]
-    non_zero_rows = np.where(row_sums!=n_cols*edge_val)[0]
+    edge_cols = np.any((image!=edge_val),axis=0)
+    edge_rows = np.any((image!=edge_val),axis=1)
+
+    non_zero_cols = np.where(edge_cols)[0]
+    non_zero_rows = np.where(edge_rows)[0]
+
     left_trim = min(non_zero_cols)
     right_trim = max(non_zero_cols)
+
     top_trim = min(non_zero_rows)
     bottom_trim = max(non_zero_rows)
+
     trimmed_image = image[top_trim:bottom_trim+1,left_trim:right_trim+1]
     return trimmed_image
 
-def otsu_threshold(image):
-    """Takes a grayscale image and binarises using Otsu's threshold.
+def otsu_threshold(
+    image
+    ):
+    """Takes a grayscale image and binarizes using Otsu's threshold.
 
     Args:
         image (numpy array): grayscale image (numpy array)
     
     Returns:
-        binary_image (numpy array): binary image (numpy array) with values in [0,1]
+        binary_image (numpy array): binary ([0,1]) image (numpy array)
     """
     threshold_val = threshold_otsu(image)
     binary_image = (image > threshold_val).astype(int)
@@ -45,40 +52,45 @@ def otsu_threshold(image):
 def extract_patches(
     image,
     patch_shape,
-    pad_val=0):
+    pad_val=0,
+    stride=None
+    ):
     """
-    Takes a 2D image array and cuts it into non-overlapping square patches of patch_shape.
-    To do this it first pads image to be exact multiples of the patch shape in each direction.
-    The image is padded with the constant pad_val, which defaults to 0
+    Takes a 2D image array and cuts it into non-overlapping square patches 
+    of patch_shape. To do this it first pads image to be exact multiples of the
+    patch shape in each direction. The image is padded with the constant 
+    pad_val, which defaults to 0
 
     Args:
         image (numpy array): numpy array of image
         patch_shape (int) : length (width) of desired square patches.
-        pad_val (int) : 
-        pad_val (int, optional): value in [0,1] that pads the binary image. Defaults to 0.
+        pad_val (int, optional): value in [0,1] that pads the binary image. 
+            efaults to 0.
+        stride (int, optional): stride across image before taking next patch. 
+            Defaults to patch_shape so patches don't overlap
 
     Returns:
         image : (array) padded image
-        patches : (array) array of patches, patches[i] will return the i^th patch.
+        patches : (array) array of patches, patches[i] will return i^th patch.
         patch_coords : list of all patch coordinates (top left per patch)
         image.shape : tuple dimensions of padded image
     """
     num_rows, num_cols = image.shape
 
     # pad image
-    if type(pad_val) != int:
-        print(f"padded value given {pad_val} is not an integer, has type {type(pad_val)}")
-    length_to_add = ceil(image.shape[0]/patch_shape)*patch_shape - image.shape[0]
-    width_to_add = ceil(image.shape[1]/patch_shape)*patch_shape - image.shape[1]
+    length_to_add = ceil(num_rows/patch_shape)*patch_shape - num_rows
+    width_to_add = ceil(num_cols/patch_shape)*patch_shape - num_cols
 
     image = np.pad(image,
-                    ((ceil(length_to_add/2),floor(length_to_add/2)), (ceil(width_to_add/2),floor(width_to_add/2))),
+                    ((ceil(length_to_add/2),floor(length_to_add/2)),
+                        (ceil(width_to_add/2),floor(width_to_add/2))),
                     'constant',
                     constant_values=(pad_val,pad_val))
     num_rows, num_cols = image.shape
     
     # take patches of padded image
-    stride=patch_shape
+    if stride is None:
+        stride=patch_shape
     patches = view_as_windows(image,
                               patch_shape,
                               stride)
@@ -88,34 +100,49 @@ def extract_patches(
     patches =  np.reshape(patches, (num_patches, patch_height, patch_width))
     
     # get the coordinates of all the patches
-    col_coords = np.array([])
-    base_coords = np.array([i*(patch_width) for i in range(int(num_cols/patch_width)+1)])
-    offsets = np.arange(0,patch_width, step=stride)
-    for offset in offsets:
-        starts = base_coords + offset
-        starts = starts[starts <= num_cols - patch_width]
-        col_coords = np.concatenate([col_coords, starts])
-    col_coords = np.array(sorted(col_coords))
-    
-    row_coords = np.array([])
-    base_coords = np.array([i*(patch_height) for i in range(int(num_rows/patch_height)+1)])
-    offsets = np.arange(0,patch_height, step=stride)
-    for offset in offsets:
-        starts = base_coords + offset
-        starts = starts[starts <= num_rows - patch_height]
-        row_coords = np.concatenate([row_coords, starts])
-    row_coords = np.array(sorted(row_coords))
-    
+    col_coords = get_coords(stride, num_cols, patch_shape)
+    row_coords = get_coords(stride, num_rows, patch_shape)
     patch_coords = list(product(row_coords, col_coords))
     
     # check number of coordinates found is the same as number of patches taken
     if patches.shape[0] != len(patch_coords):
-        print(f"{patches.shape[0]} patches found, {patch_coords} coordinates found")
+        print(f"{patches.shape[0]} patches found,\
+         {patch_coords} coordinates found")
     return image, patches, patch_coords, image.shape
 
+def get_coords(
+    stride,
+    axis_size,
+    patch_shape
+    ):
+    """Calculates top left patch coordinate for image with axis size length,
+    with patch_shape sized patches.
+
+    Args:
+        stride (int): stride between patches
+        axis_size (int): length of axis to cut into patches
+        patch_shape (int): length of patch
+
+    Returns:
+        numpy array: coordinates along axis of patches length patch_shape,
+            stride distance apart 
+    """
+    coords = np.array([])
+    base_coords = np.array(
+        [i*(patch_shape) for i in range(int(axis_size/patch_shape)+1)]
+        )
+    offsets = np.arange(0,patch_shape, step=stride)
+    for offset in offsets:
+        starts = base_coords + offset
+        starts = starts[starts <= axis_size - patch_shape]
+        coords = np.concatenate([coords, starts])
+
+    coords = np.array(sorted(coords))
+    return coords
 
 def SEDT(
-    image):
+    image
+    ):
     """
     Calculates a Signed Euclidean Distance Transform of an image array.
 
@@ -124,13 +151,14 @@ def SEDT(
 
     Returns:
         sedt_image : (numpy array) SEDT of image.
-
     """
     inverse_image = np.logical_not(image).astype(int)
     edt_1_to_0 = ndimage.distance_transform_edt(image)
     edt_0_to_1 = ndimage.distance_transform_edt(inverse_image)
+
     # negative distances for 0 (pore/air) to 1 (material)
     edt_0_to_1 = - edt_0_to_1
+
     # where image is True (material) replace the distance 
     # with distance from material to pore/air
     neg = np.multiply(inverse_image, edt_0_to_1)
--- ut_preprocessing_images.py
+++ ut_preprocessing_images.py
@@ -1,6 +1,7 @@
 import numpy as np
 from importlib import reload
 from itertools import product
+from math import sqrt
 
 import preprocessing_images as pre_process
 reload(pre_process)
@@ -8,20 +9,38 @@ reload(pre_process)
 # TEST - trim
 ######################################################
 # basic test image 
-test_im_0_edges =  np.array([[0,0,0,0],[0,1,2,0],[0,3,4,0],[0,0,0,0]])
+test_im_0_edges =  np.array([
+    [0,0,0,0,0],
+    [0,1,2,0,0],
+    [0,3,4,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0]]
+    )
 trim_test_im_0_edges = np.array([[1,2],[3,4]])
-test_im_1_edges =  np.array([[1,1,1,1],[1,3,2,1],[1,3,4,1],[1,1,1,1]])
+test_im_1_edges =  np.array([
+    [1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,3,2,1,1],
+    [1,3,4,1,1],
+    [1,1,1,1,1]]
+    )
 trim_test_im_1_edges = np.array([[3,2],[3,4]])
 
 #check shape matches
-assert pre_process.trim(test_im_0_edges,edge_val=0).shape == trim_test_im_0_edges.shape, "Shape doesn't match"
-assert pre_process.trim(test_im_1_edges,edge_val=1).shape == trim_test_im_1_edges.shape, "Shape doesn't match"
+assert pre_process.trim(test_im_0_edges,edge_val=0).shape ==\
+    trim_test_im_0_edges.shape, "Shape doesn't match"
+assert pre_process.trim(test_im_1_edges,edge_val=1).shape ==\
+    trim_test_im_1_edges.shape, "Shape doesn't match"
 #check values match
-assert (pre_process.trim(test_im_0_edges,edge_val=0) == trim_test_im_0_edges).all(), "trim failed unit test, zero background value"
-assert (pre_process.trim(test_im_1_edges, edge_val=1) == trim_test_im_1_edges).all(), "trim failed unit test, non-zero background value"
+assert (pre_process.trim(test_im_0_edges,edge_val=0) ==\
+    trim_test_im_0_edges).all(), "trim failed unit test, zero background value"
+assert (pre_process.trim(test_im_1_edges, edge_val=1) ==\
+    trim_test_im_1_edges).all(), "trim failed unit test, non-zero background value"
 
 # TEST - threshold otsu
 ######################################################
+rt2 = sqrt(2)
+rt5 = sqrt(5)
 test_image =  np.array([
     [rt2,rt2,rt5,rt5],
     [rt2,rt2,rt5,rt5],
@@ -35,9 +54,12 @@ test_binary = np.array([
 
 binary_image, threshold_val = pre_process.otsu_threshold(test_image)
 
-assert (test_binary.shape == binary_image.shape), "otsu threshold, test failed binary image incorrect shape"
-assert (set(list(binary_image.flatten())) == {0,1}), "otsu threshold, test failed binary has values other than {0,1}"
-assert (test_binary == binary_image).all(), "otsu threshold, test failed binary doesn't match"
+assert (test_binary.shape == binary_image.shape),\
+    "otsu threshold, test failed binary image incorrect shape"
+assert (set(list(binary_image.flatten())) == {0,1}),\
+    "otsu threshold, test failed binary has values other than {0,1}"
+assert (test_binary == binary_image).all(),\
+    "otsu threshold, test failed binary doesn't match"
 
 # TEST  - extract_patches
 ######################################################
@@ -63,14 +85,23 @@ test_patches = np.array([
     test_padded_image[3:6,0:3:],
     test_padded_image[3:6,3:6],
     test_padded_image[3:6,6:9]])
-padded_image, patches, coords, shape = pre_process.extract_patches(test_input_im, patch_shape=3, pad_val=0)
+padded_image, patches, coords, shape = pre_process.extract_patches(
+    test_input_im,
+    patch_shape=3,
+    pad_val=0
+    )
 
-assert padded_image.shape == test_padded_image.shape, "extract patches padded to incorrect shape"
-assert (padded_image.shape[0]%3 == 0) and (padded_image.shape[1]%3 == 0), "failed to pad to integer multiple of patch shape"
-assert (test_padded_image == padded_image).all(), "extract patches padded with incorrect value"
+assert padded_image.shape == test_padded_image.shape,\
+    "extract patches padded to incorrect shape"
+assert (padded_image.shape[0]%3 == 0) and (padded_image.shape[1]%3 == 0),\
+    "failed to pad to integer multiple of patch shape"
+assert (test_padded_image == padded_image).all(),\
+    "extract patches padded with incorrect value"
 for i in range(len(test_patches)):
-    assert (test_coords[i] == coords[i]), "extract patches coordinates don't match"
-    assert (test_patches[i] == patches[i]).all(), "extract patches failed to extract correct patches"
+    assert (test_coords[i] == coords[i]),\
+        "extract patches coordinates don't match"
+    assert (test_patches[i] == patches[i]).all(),\
+        "extract patches failed to extract correct patches"
 
 # TEST  - SEDT (Signed Euclidean distance transform)
 ######################################################
@@ -80,19 +111,21 @@ test_input_im = np.array([
     [1,0,0,0,1,1,1],
     [1,1,0,0,0,0,0],
     [1,1,1,0,0,1,0],
-    [1,1,0,0,0,0,0]])
-from math import sqrt
-rt2 = sqrt(2)
-rt5 = sqrt(5)
+    [1,1,0,0,0,0,0]]
+    )
 test_output_im = np.array([
     [1,-1,-2,-1,1,1,rt2],
     [1,-1,-2,-1,1,-1,1],
     [1,-1,-rt2,-1,1,1,1],
     [rt2,1,-1,-rt2,-1,-1,-1],
     [rt5,rt2,1,-1,-1,1,-1],
-    [2,1,-1,-rt2,-rt2,-1,-rt2]])
+    [2,1,-1,-rt2,-rt2,-1,-rt2]]
+    )
 
 #check the shape isn't changed
-assert pre_process.SEDT(test_input_im).shape == test_input_im.shape, "SEDT changed shape of test image"
+assert pre_process.SEDT(test_input_im).shape == test_input_im.shape,\
+    "SEDT changed shape of test image"
 # check the distance values are correct for test image
-assert(pre_process.SEDT(test_input_im) == test_output_im).all(), "SEDT function failed test"
+assert(pre_process.SEDT(test_input_im) == test_output_im).all(),\
+    "SEDT function failed test"
+    
